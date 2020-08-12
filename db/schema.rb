ba61eec21103d5bf5f79982dce06b4ef10009800
Raul Almanza <ra.almanzaserrano@gmail.com>
2020-08-11 22:30:09 -0500

    Establish relationships between modules and write seeds to populate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_220024) do
+ActiveRecord::Schema.define(version: 2020_08_12_032725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_220024) do
     t.datetime "end_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "employee_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CompanyBranch.create(name: 'Company Inc', address: 'Boulevard Mariano Escobedo #3423')
+CompanyBranch.create(name: 'Company Inc Robledo', address: 'Avenida Robledo #205')
+CompanyBranch.create(name: 'Company Inc Saturno', address: 'Boulevard Saturno #7465')
+
+Employee.create(name: 'Raul Almanza', email: 'raul.almanza@companyinc.com', position: 'Developer', private_number: 1029384, company_branch_id: 1)
+Employee.create(name: 'Andrea Silva', email: 'andrea.silva@companyinc.com', position: 'Project Manager', private_number: 1239485, company_branch_id: 2)
+Employee.create(name: 'Eduardo Hernandez', email: 'eduardo.hernandez@companyinc.com', position: 'Developer', private_number: 1023333, company_branch_id: 3)
+Employee.create(name: 'Roberto Gil', email: 'roberto.gil@companyinc.com', position: 'Tech Lead', private_number: 1024444, company_branch_id: 1)
+Employee.create(name: 'Esmeralda Fritz', email: 'esmeralda.fritz@companyinc.com', position: 'Project Manager', private_number: 1025555, company_branch_id: 2)
+Employee.create(name: 'Anmeen Leong', email: 'anmeen.leong@companyinc.com', position: 'Project Lead', private_number: 1026666, company_branch_id: 3)
+Employee.create(name: 'Neville Davey', email: 'neville.davey@companyinc.com', position: 'Project Lead', private_number: 1027777, company_branch_id: 1)
